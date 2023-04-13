74e90010e4f4dd2a086db599cee61584a083309c
tommy_s4cc9e2 <tommyofwilsons@gmail.com>
2023-04-12 20:29:52 -0700

    no longer resizable

--- desktop/src/page/rightshift/rtst1/DesktopLauncher.java
+++ desktop/src/page/rightshift/rtst1/DesktopLauncher.java
@@ -10,6 +10,7 @@ public class DesktopLauncher {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
 		config.setTitle("AIRline");
+		config.setResizable(false);
 		new Lwjgl3Application(new Game(), config);
 	}
 }
